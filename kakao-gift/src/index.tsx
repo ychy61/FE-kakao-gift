925c652708e45c5ee23d4ad695093680455f6a58
ychy61 <ychy010601@gmail.com>
2024-06-27 16:10:18 +0900

    reset css를 적용 emotion 스타일 라이브러리를 추가하고 reset css를 적용함 https://meyerweb.com/eric/tools/css/reset/ 위 링크 참고

--- kakao-gift/src/index.tsx
+++ kakao-gift/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './GlobalStyles'; // reset CSS 파일 import
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyles /> {/* reset CSS 적용 */}
     <App />
   </React.StrictMode>
 );
